22c68493d8ea87b0e67ff1da2f58d03c901b9a33
chiovcr <cristian.chiovari@gmail.com>
2021-02-05 14:50:30 +0100

    SpringBoot project init

---
+++ user-rest/src/test/java/ccs/pocs/user/rest/userrest/UserRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ccs.pocs.user.rest.userrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
